e2b758197c05d380141a788fa716d0e22cc46515
noceive <nocivescience@gmail.com>
2022-06-27 23:25:22 -0400

    horrible efecto

--- added_anims.py
+++ added_anims.py
@@ -1,1 +1,20 @@
+from this import d
 from manim import *
+import itertools as it
+class TwoCase(Scene):
+    def construct(self):
+        anims=self.all_anims(self.get_triangles())
+        self.play(anims)
+        self.wait()
+    def get_anims(self):
+        dot=Dot()
+        return Create(dot)
+    def other_animations(self):
+        circle=Circle()
+        return FadeIn(circle)
+    def get_triangles(self):
+        triangle=Triangle()
+        return [GrowFromCenter(triangle)]
+    def all_anims(self,*added_anims):
+        anims=it.chain(self.get_anims(),*added_anims)
+        return anims
